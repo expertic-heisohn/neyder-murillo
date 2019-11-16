834e6aa2ad604e92c2e5f0f970df2b7214f85c66
neyder <neydermurillo@hotmail.com>
2019-11-15 23:48:34 -0500

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { HelloWorld } from '../components/hello-world/hello-world.component'
-
+import { EventComponent } from '../components/event/event.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorld
+    EventComponent
   ],
   imports: [
     BrowserModule,
