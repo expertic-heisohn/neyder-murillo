5ab5933e624b762c156f00bbd63231d086204ce4
neyder <neydermurillo@hotmail.com>
2019-11-16 14:14:21 -0500

    ngFor and compenentes

--- src/components/event/event.component.ts
+++ src/components/event/event.component.ts
@@ -1,9 +1,9 @@
-import {Component} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
 })
 export class EventComponent {
-
+  @Input() value: any;
 }
